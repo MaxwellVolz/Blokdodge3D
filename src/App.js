1de95c73c060d73c068f5389b26bac1882e316cf
MaxwellVolz <maxwellvolz@gmail.com>
2021-08-17 21:46:08 -0700

    crazy way too many dependencies box falling example

--- src/App.js
+++ src/App.js
@@ -1,38 +1,49 @@
-import React, { useRef, useState } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
+// import { Box, Plane } from "@react-three/drei";
+import { useEffect, useState, useRef } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { Physics, usePlane, useBox } from '@react-three/cannon'
+import './App.css'
 
-function Box(props) {
-  // This reference will give us direct access to the mesh
-  const ref = useRef()
-  // Set up state for the hovered and active state
-  const [hovered, setHover] = useState(false)
-  const [active, setActive] = useState(false)
-  // Rotate mesh every frame, this is outside of React without overhead
-  useFrame(() => {
-    ref.current.rotation.x = ref.current.rotation.y += 0.01
-  })
+
+function Plane(props) {
+  const [ref] = usePlane(() => ({ rotation: [-Math.PI / 2, 0, 0], ...props }))
   return (
-    <mesh
-      {...props}
-      ref={ref}
-      scale={active ? 1.5 : 1}
-      onClick={(e) => setActive(!active)}
-      onPointerOver={(e) => setHover(true)}
-      onPointerOut={(e) => setHover(false)}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    <mesh receiveShadow ref={ref}>
+      <planeGeometry args={[1000, 1000]} />
+      <meshStandardMaterial color="#f0f0f0" />
+    </mesh>
+  )
+}
+
+function Cube(props) {
+  const [ref] = useBox(() => ({ mass: 1, ...props }))
+  return (
+    <mesh castShadow ref={ref}>
+      <boxGeometry />
+      <meshStandardMaterial color="orange" />
     </mesh>
   )
 }
 
 export default function App() {
+  const [ready, set] = useState(false)
+  useEffect(() => {
+    const timeout = setTimeout(() => set(true), 1000)
+    return () => clearTimeout(timeout)
+  }, [])
   return (
-    <Canvas>
-      <ambientLight intensity={0.5} />
-      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-      <pointLight position={[-10, -10, -10]} />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
+    <Canvas dpr={[1, 2]} shadows camera={{ position: [-5, 5, 5], fov: 50 }}>
+      <ambientLight />
+      <spotLight angle={0.25} penumbra={0.5} position={[0, 20, 0]} castShadow />
+      <Physics gravity={[0, -2, 0]}>
+        <Plane />
+        <Cube position={[3.5, 16, 0]} />
+        <Cube position={[0, 20, -0.25]} />
+        <Cube position={[-3.5, 25, 0.25]} />
+        {ready && <Cube position={[-0.45, 40, 2.25]} />}
+        {ready && <Cube position={[-3.45, 42, 0.25]} />}
+        {ready && <Cube position={[3.45, 40, -2.25]} />}
+      </Physics>
     </Canvas>
   )
 }
